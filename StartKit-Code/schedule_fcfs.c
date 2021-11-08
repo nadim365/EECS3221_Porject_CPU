9fa2397d850fc8c071342531b49c32e0b3bc0ff0
nadim <samnad14@gmail.com>
2021-11-07 23:05:07 -0500

    added lines to schedule function in schedule_fcfs

--- StartKit-Code/schedule_fcfs.c
+++ StartKit-Code/schedule_fcfs.c
@@ -6,26 +6,34 @@
 #include "cpu.h"
 //code to run first come first served scheduling algorithm
 struct node *head = NULL;
-
+int id = 0;
 //adds tasks into linked list
 void add(char *name, int priority, int burst) {
 Task t;
 t.name = name;
-//t.tid =
+t.tid = id;
 t.priority = priority;
 t.burst = burst
 insert(&head,t); 
+id = id + 1;
 
 }
 
 void schedule() {
-    int turnaroundTime;
-    int waitTime;
-    int responseTime;
+    int avg_turn;
+    int avg_wait;
+    int avg_res;
     struct node *temp;
     temp = head;
 
     while(temp.next != NULL){
         run(temp->task, temp->task->burst);
+        if(temp->task->tid != 0){
+            for (int i = 0; i < temp->task->tid; i++)
+            {
+                avg_wait = avg_wait + temp->task->burst;
+            }
+        }
+
     }
 }
