0e33a9d3230552b98435eead732989fca4c7e77a
nadim <samnad14@gmail.com>
2021-11-10 22:29:27 -0500

    updated formula for wait times

--- StartKit-Code/schedule_fcfs.c
+++ StartKit-Code/schedule_fcfs.c
@@ -19,22 +19,26 @@ id = id + 1;
 }
 
 void schedule() {
-    float avg_turn = 0;
-    float avg_wait = 0;
-    float avg_res = 0;
+    float avg_turn = 0.0;
+    float avg_wait = 0.0;
+    float wait = 0.0;
+    float avg_res = 0.0;
     struct node *temp;
     int len = list_len(head);
     temp = head;
+    //avg_wait = temp->task->burst;
     while(temp != NULL){
         run(temp->task, temp->task->burst);
+        //avg_wait = avg_wait + (avg_turn + temp->task->burst) ;
         avg_turn = avg_turn + avg_wait + temp->task->burst;
+        wait = wait + avg_wait;
         avg_wait = avg_wait + temp->task->burst;
         avg_res = avg_wait;
         temp = temp->next;
     }
        avg_turn = avg_turn / ((float) len) ;
-       avg_wait = avg_wait / ((float) len) ;
-       avg_res = avg_res / ((float) len) ;
+       avg_wait = wait / ((float) len) ;
+       avg_res = wait / ((float) len) ;
      printf("Average waiting time = %f \n", avg_wait);
      printf("Average turnaround time = %f \n", avg_turn);
      printf("Average response time = %f \n", avg_res);
