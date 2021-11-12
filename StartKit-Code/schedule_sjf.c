731355d34ec7b9785b9db8caa526da18689378bf
nadim <samnad14@gmail.com>
2021-11-12 00:19:19 -0500

    made changes to schedule function

--- StartKit-Code/schedule_sjf.c
+++ StartKit-Code/schedule_sjf.c
@@ -8,6 +8,7 @@
 //code to run shortest job first scheduling algorithm
 struct node *head = NULL;
 int id = 0;
+
 //adds tasks into linked list
 void add(char *name, int priority, int burst) {
 Task *t = malloc(sizeof(struct task)) ;
@@ -15,7 +16,7 @@ t->name = name;
 t->tid = id;
 t->priority = priority;
 t->burst = burst;
-insert_last(&head,t);
+insert(&head,t);
 id = id + 1;
 }
 
@@ -26,6 +27,9 @@ void schedule(){
     float wait = 0.0;
     struct node *temp;
     int len = list_len(head);
+    //traverse(head);
+    //sort_list(head);
+    sort_list(head);
     sort_list(head);
     temp = head;
     
@@ -46,5 +50,5 @@ void schedule(){
     printf("Average waiting time = %f \n", avg_wait);
     printf("Average turnaround time = %f \n", avg_turn);
     printf("Average response time = %f \n", avg_res);
-
+    //traverse(head);
 }
