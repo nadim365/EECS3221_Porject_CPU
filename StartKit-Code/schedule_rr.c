9c9c4e995a5293b04ed9bb34b5c97a42398f4a38
nadim <samnad14@gmail.com>
2021-11-12 23:59:05 -0500

    added lines to rr schedule file

---
+++ StartKit-Code/schedule_rr.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "schedulers.h"
+#include "list.h"
+#include "task.h"
+#include "cpu.h"
+#include "pArr.h"
+
+
+
+//code to run shortest job first scheduling algorithm
+struct node *head = NULL;
+struct arr *head_1 = NULL;
+int id = 0;
+int counter = 0;
+
+
+//adds tasks into linked list
+void add(char *name, int priority, int burst) {
+Task *t = malloc(sizeof(struct task)) ;
+struct arr *a = malloc(sizeof(struct arr));
+t->name = name;
+t->tid = id;
+t->priority = priority;
+t->burst = burst;
+a->rem_burst = burst;
+a->tid = id;
+insert_arr(&head_1, a);
+insert_last(&head,t);
+id = id + 1;
+counter = counter + 1; 
+}
+
+void schedule(){
+    float avg_turn = 0.0;
+    float avg_wait = 0.0;
+    float wait = 0.0;
+    float avg_res = 0.0;
+    struct node *temp;
+    int len = list_len(head);
+    int i;
+    
+    //avg_wait = temp->task->burst;
+    for(i = 0; i < len ; i++){
+
+        temp = head;
+
+        while(temp != NULL)
+        {
+
+         run(temp->task, temp->task->burst - QUANTUM);
+         //avg_wait = avg_wait + (avg_turn + temp->task->burst) ;
+         avg_turn = avg_turn + avg_wait + temp->task->burst;
+         wait = wait + avg_wait;
+         avg_wait = avg_wait + temp->task->burst;
+         avg_res = avg_wait;
+         temp = temp->next;
+      }
+    }
+       
+    avg_turn = avg_turn / ((float) len) ;
+    avg_wait = wait / ((float) len) ;
+    avg_res = wait / ((float) len) ;
+    printf("Average waiting time = %f \n", avg_wait);
+    printf("Average turnaround time = %f \n", avg_turn);
+    printf("Average response time = %f \n", avg_res);
+     
+}
