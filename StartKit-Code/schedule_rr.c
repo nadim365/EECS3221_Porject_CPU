ab7ad477a34777b09b32be1c81371dc9faf41f26
nadim <samnad14@gmail.com>
2021-11-15 23:00:01 -0500

    refactored schedule function for round robin

--- StartKit-Code/schedule_rr.c
+++ StartKit-Code/schedule_rr.c
@@ -27,7 +27,7 @@ void add(char *name, int priority, int burst){
      max_burst = burst;
  }
 
- //insert_arr(&head_1, a);
+ 
  insert_last(&head,t);
  id = id + 1;
  counter = counter + 1; 
@@ -38,13 +38,11 @@ void add(char *name, int priority, int burst){
 void schedule(){
     float avg_turn = 0.0;
     float avg_wait = 0.0;
-    //float wait = 0.0;
-    //float turn = 0.0;
     float avg_res = 0.0;
     struct node *temp;
     int looper = max_burst/QUANTUM;
     int len = list_len(head);
-    int rem_burst[len];
+    int burst[len];
     int wt[len];
     int i,j;
     int t = 0; //time elapsed
@@ -53,7 +51,7 @@ void schedule(){
     //initializing array with burst times of processes
     for(j= 0; j < len; j++)
     {
-        rem_burst[j] = temp->task->burst;
+        burst[j] = temp->task->burst;
         wt[j] = 0;
         temp = temp->next; 
     }
@@ -61,75 +59,57 @@ void schedule(){
     
     for(i = 0; i < looper ; i++)
     {
-
         temp = head;
 
         while(temp != NULL)
         {
-           //last stage of process
-            if ( (rem_burst[temp->task->tid] > 0) && (rem_burst[temp->task->tid] < QUANTUM) )
-            {
-                
-                
-                run(temp->task, rem_burst[temp->task->tid]);
-                //rem_burst[temp->task->tid] = 0;
-                t = t + rem_burst[temp->task->tid];
-                  //wait = t - temp->task->burst;
-                //turn = wait + temp->task->burst;
-                //avg_turn = avg_turn + temp->task->burst + wait;
-                //avg_turn = avg_turn + turn;
-                
-                
-                //avg_wait = avg_wait + wait;
-                rem_burst[temp->task->tid] = 0;
-                
-
-            }
-            //process finished executing
-            else if (rem_burst[temp->task->tid] == 0 || rem_burst[temp->task->tid] < 0)
-            {
-                //t = t + rem_burst[temp->task->tid];
-                //wait = t - temp->task->burst;
-                //avg_turn = avg_turn + temp->task->burst + wait;
-                //avg_wait = avg_wait + wait;
-                //wait = t - temp->task->burst;
-                //avg_wait = wait + avg_wait;
-                wt[temp->task->tid] = t - temp->task->burst;
-                temp = temp->next;
-                continue;
-            }
-            //process has just started executing
-            else{
-                //printf("third if \n");
-                run(temp->task, rem_burst[temp->task->tid]);
-                t = t + QUANTUM;
-                rem_burst[temp->task->tid] = rem_burst[temp->task->tid] - QUANTUM;
-            }
-
-         //avg_wait = avg_wait + (avg_turn + temp->task->burst);
-         //avg_turn = avg_turn + avg_wait + temp->task->burst;
-         //wait = wait + avg_wait;
-         //avg_wait = avg_wait + wait;
+            //process starts execution
+           if(temp->task->burst >= QUANTUM){
+               run(temp->task, QUANTUM);
+               avg_turn = avg_turn + temp->task->burst;
+               temp->task->burst = temp->task->burst - QUANTUM;
+               t = t + QUANTUM;
+           }
+           //last stage of execution
+           else if (temp->task->burst < QUANTUM && temp->task->burst > 0)
+           {
+               run(temp->task, temp->task->burst);
+               t = t + temp->task->burst;
+               wt[temp->task->tid] = t ;
+               printf("first wait : %d \n", wt[temp->task->tid]);
+               temp->task->burst = 0; 
+               temp = temp->next;
+               continue;
+           }
+           //process finished executing
+           else
+           {
+               if(temp->task->burst == 0 )
+               {
+                        wt[temp->task->tid] = t - temp->task->burst;
+                        printf("second wait : %d \n", t - temp->task->burst);
+                  
+               }
+           }
          avg_res = avg_wait;
          temp = temp->next;
       }
     }
-
+    //summing up the wait times 
     for (i = 0; i < len; i++)
     {
-        avg_wait = avg_wait + wt[i];
- 
+        avg_wait = avg_wait + wt[i];  
     }
-       
+    avg_turn = avg_turn + avg_wait;      
     avg_turn = avg_turn / ((float) len) ;
     avg_wait = avg_wait / ((float) len) ;
     avg_res = avg_wait / ((float) len) ;
+    printf("time = %d \n", t);
     printf("Average waiting time = %f \n", avg_wait);
     printf("Average turnaround time = %f \n", avg_turn);
     printf("Average response time = %f \n", avg_res);
      
 }
-
 
 /*
 void schedule(){
